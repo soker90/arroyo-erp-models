4c6cabdc30b2a270312a6496dfcbe140c8182d39
Eduardo Parra <edue17@gmail.com>
2020-05-22 21:27:44 +0200

    update delivery order pre save hook

--- models/deliveryorder/hooks/calc-fields-pre-save.js
+++ models/deliveryorder/hooks/calc-fields-pre-save.js
@@ -1,14 +1,28 @@
 async function calcFieldsPreSave(next) {
-  this.size = this.products.length;
-  this.total = this.products.reduce((accumulator, currentValue) => accumulator + currentValue);
+  if (!this.products) this.products = [];
 
-  const { name } = await ProviderModel.findOne({ _id: this.provider });
+  this.size = this.products.length;
+  this.rate = 0;
+  this.iva = 0;
+  this.re = 0;
+  this.total = 0;
+  this.selectedProducts = [];
+
+  this.products.forEach(({product, rate, iva, re, total}) => {
+    if (rate) this.rate += rate;
+    if (iva) this.iva += iva;
+    if (re) this.re += re;
+    if (total) this.total += total;
+    this.selectedProducts.push(product);
+  })
+
+  const {name} = await ProviderModel.findOne({_id: this.provider});
   if (name) {
     this.nameProvider = name;
-    next();
   } else {
     next(new Error('No se ha encontrado el provedor'));
   }
+  next();
 }
 
 module.exports = calcFieldsPreSave;
