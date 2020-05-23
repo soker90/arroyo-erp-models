d9ab2620de7e6424fa8a4877621cc4b2261f14e9
Eduardo Parra <edue17@gmail.com>
2020-05-23 23:04:08 +0200

    improvements in pre save

--- models/deliveryorder/hooks/calc-fields-pre-save.js
+++ models/deliveryorder/hooks/calc-fields-pre-save.js
@@ -3,14 +3,10 @@ async function calcFieldsPreSave(next) {
   if (!this.products) this.products = [];
 
   this.size = this.products.length;
-  this.rate = 0;
-  this.iva = 0;
-  this.re = 0;
-  this.total = 0;
+  this.iva = this.re = this.total = 0;
   this.selectedProducts = [];
 
-  this.products.forEach(({product, rate, iva, re, total}) => {
-    if (rate) this.rate += rate;
+  this.products.forEach(({product, iva, re, total}) => {
     if (iva) this.iva += iva;
     if (re) this.re += re;
     if (total) this.total += total;
