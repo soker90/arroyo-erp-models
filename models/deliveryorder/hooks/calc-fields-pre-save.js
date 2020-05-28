21840acf8054a879d92d6e087e18a8b0b6c96407
Eduardo Parra <edue17@gmail.com>
2020-05-28 21:55:39 +0200

    fix

--- models/deliveryorder/hooks/calc-fields-pre-save.js
+++ models/deliveryorder/hooks/calc-fields-pre-save.js
@@ -5,18 +5,26 @@ async function calcFieldsPreSave(next) {
   this.size = this.products.length;
   this.iva = this.re = this.total = 0;
 
-  this.products.forEach(({product, iva, re, total}) => {
+  this.products.forEach(({iva, re, total}) => {
+    /**
+     * Se calcula en el servicio
+     * products[i].re = price * quantity * product.re
+     * products[i].iva = price * quantity * product.iva
+     * products[i].total = products[i].iva + products[i].re + (price * quantity)
+     */
     if (iva) this.iva += iva;
     if (re) this.re += re;
     if (total) this.total += total;
   });
 
-  const {name} = await ProviderModel.findOne({_id: this.provider});
+  if(!this.nameProvider) {
+    const { name } = await ProviderModel.findOne({ _id: this.provider });
 
-  if (name) {
-    this.nameProvider = name;
-  } else {
-    next(new Error('No se ha encontrado el provedor'));
+    if (name) {
+      this.nameProvider = name;
+    } else {
+      next(new Error('No se ha encontrado el provedor'));
+    }
   }
   next();
 }
